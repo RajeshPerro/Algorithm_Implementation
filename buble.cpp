b8535e11fcfc3664fc30d4ac4d48703b1056cdaf
rajesh <rajeshghosh4007@gmail.com>
2016-12-29 13:01:22 +0100

    bubble sort oop done

--- buble.cpp
+++ buble.cpp
@@ -1,32 +1,59 @@
-#include<stdio.h>
+#include "iostream"
 
-int main()
+using namespace std;
+
+class buble
 {
-  int a[100],i,temp,j,n;
-  printf("how many el elements: ");
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
+  bool swaaped;
+  int j;
+  int temp;
+public:
+  void bubbleSort(int* , int);
+  
+};
+void buble::bubbleSort(int arra[], int n )
+{
+  swaaped = true; j = 0; 
+  while(swaaped)
   {
-    scanf("%d",&a[i]);
-  }
-
-  for(i=0;i<n-1;i++)
-  {
-    for(j=0;j<n-i;j++)
+    swaaped = false;
+    j++;
+    for (int i = 0; i < n-j; ++i)
     {
-        if(a[j]>a[j+1])
-        {
-           temp=a[j];
-           a[j]=a[j+1];
-           a[j+1]=temp;
-           }
-  }
-}
-    for(i=0;i<n;i++)
-    {
-      printf("%d\n",a[i]);
+      if (arra[i] > arra[i+1])
+      {
+        temp = arra[i];
+        arra[i] = arra[i+1];
+        arra[i+1] = temp;
+        swaaped = true;  
+      }
     }
-
-return 0;
+  }
+  for (int i = 0; i < n; ++i)
+  {
+    cout<<arra[i]<<" ";
+  }
 }
 
+int main(int argc, char const *argv[])
+{
+  int num, arr[100];
+  buble bsort;
+  cout<<"Enter the number of elements : ";
+  cin >> num;
+  for (int i = 0; i < num; ++i)
+  {
+    arr[i]=(i*num)+(num-i)*(num+i);
+    //cin>>arr[i];
+  }
+  cout<<"Array Elements are : ";
+  for (int i = 0; i < num; ++i)
+  {
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  cout<<"After operatating BubbleSort : ";
+  bsort.bubbleSort(arr,num);
+  cout<<endl;
+  return 0;
+}
